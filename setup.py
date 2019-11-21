ed285366c62fb13dc06eb681b0aa3e066c7fa39e
61246880 <jorge.plautz@gmail.com>
2019-11-20 21:50:47 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ def read(fname):
 # of replicating them:
 standard_exclude = ["*.py", "*.pyc", "*$py.class", "*~", ".*", "*.bak"]
 standard_exclude_directories = [
-    ".*", "CVS", "_darcs", "./build", "./dist", "EGG-INFO", "*.egg-info"
-]
+    ".*", "CVS", "_darcs", "./build", "./dist", "EGG-INFO", "*.egg-info"]
 
 
 # (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
@@ -25,35 +24,28 @@ standard_exclude_directories = [
 # you can't import this from another package, when you don't know if
 # that package is installed yet.
 def find_package_data(
-        where=".",
-        package="",
-        exclude=standard_exclude,
-        exclude_directories=standard_exclude_directories,
-        only_in_packages=True,
-        show_ignored=False):
+    where=".",
+    package="",
+    exclude=standard_exclude,
+    exclude_directories=standard_exclude_directories,
+    only_in_packages=True,
+    show_ignored=False):
     """
     Return a dictionary suitable for use in ``package_data``
     in a distutils ``setup.py`` file.
-
     The dictionary looks like::
-
         {"package": [files]}
-
     Where ``files`` is a list of all the files in that package that
     don"t match anything in ``exclude``.
-
     If ``only_in_packages`` is true, then top-level directories that
     are not packages won"t be included (but directories under packages
     will).
-
     Directories matching any pattern in ``exclude_directories`` will
     be ignored; by default directories with leading ``.``, ``CVS``,
     and ``_darcs`` will be ignored.
-
     If ``show_ignored`` is true, then all the files that aren"t
     included in package data are shown on stderr (for debugging
     purposes).
-
     Note patterns use wildcards, or can be exact paths (including
     leading ``./``), and all searching is case-insensitive.
     """
@@ -66,16 +58,18 @@ def find_package_data(
             if os.path.isdir(fn):
                 bad_name = False
                 for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print(sys.stderr, (
-                                    "Directory %s ignored by pattern %s"
-                                    % (fn, pattern)))
+                            print >> sys.stderr, (
+                                "Directory %s ignored by pattern %s"
+                                % (fn, pattern))
                         break
                 if bad_name:
                     continue
-                if (os.path.isfile(os.path.join(fn, "__init__.py")) and not prefix):
+                if (os.path.isfile(os.path.join(fn, "__init__.py"))
+                    and not prefix):
                     if not package:
                         new_package = name
                     else:
@@ -87,26 +81,28 @@ def find_package_data(
                 # is a file
                 bad_name = False
                 for pattern in exclude:
-                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print(sys.stderr, (
-                                    "File %s ignored by pattern %s"
-                                    % (fn, pattern)))
+                            print >> sys.stderr, (
+                                "File %s ignored by pattern %s"
+                                % (fn, pattern))
                         break
                 if bad_name:
                     continue
-                out.setdefault(package, []).append(prefix + name)
+                out.setdefault(package, []).append(prefix+name)
     return out
 
 
 PACKAGE = "libpythonjp"
 NAME = PACKAGE
-DESCRIPTION = "Modulo para exemplificar construção de projetos Python para curso Pythontools"
+DESCRIPTION = "Modulo para exemplificar construcapipo de projetos Python para curso Pythontools"
 AUTHOR = "Jorge Luiz Plautz"
 AUTHOR_EMAIL = "jorge.plautz@gmail.com"
 URL = "https://github.com/pythonprgbr/libpythonjp"
 VERSION = __import__(PACKAGE).__version__
+
 
 setup(
     name=NAME,
@@ -116,7 +112,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('LICENSE'),
+    license="GNU AFFERO GENERAL PUBLIC LICENSE",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
